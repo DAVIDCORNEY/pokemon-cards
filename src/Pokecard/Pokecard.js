aa5ff96d2e56dba7acfefba761f9a9a701d5dba5
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-07-30 14:40:28 +0100

    Pokecard add pokemon image,name,type,exp

--- src/Pokecard/Pokecard.js
+++ src/Pokecard/Pokecard.js
@@ -1,7 +1,17 @@
 import React from "react";
+const POKEMON_API =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-function Pokecard() {
-  return <h1>Pokecard</h1>;
+function Pokecard({ name, id, type, exp }) {
+  const imgSrc = `${POKEMON_API}${id}.png`;
+  return (
+    <div className="Pokecard">
+      <h1>{name}</h1>
+      <img src={imgSrc} alt={name} />
+      <div>{type}</div>
+      <div>{exp}</div>
+    </div>
+  );
 }
 
 export default Pokecard;
