3a43316f76a4989068ba82fcc08270e77d7a3f8a
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-07-30 22:27:24 +0100

    setup Pokegame component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Pokedex from "./Pokedex/Pokedex";
+import Pokegame from "./Pokegame/Pokegame";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <Pokegame />
     </div>
   );
 }
